7a954348c0b847066afe4a2a19bcb659b8791b34
Michael Lozano <michaellozano@Michaels-Mac-Studio.local>
2023-09-27 07:02:41 -0500

    Lozano: Adding project init

---
+++ src/main/java/com/lozano/Showcase_Security_Service/ShowcaseSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lozano.Showcase_Security_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowcaseSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lozano/Showcase_Security_Service/ShowcaseSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lozano.Showcase_Security_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowcaseSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
